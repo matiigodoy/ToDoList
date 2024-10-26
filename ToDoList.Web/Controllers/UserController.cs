5bb99ffda738c0144963be1c6be37e0084c68223
Santiago <142843041+SantiagoTucci@users.noreply.github.com>
2024-10-26 12:49:47 -0300

    cambios

---
+++ ToDoList.Web/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Web.Controllers
+{
+    public class UserController : Controller
+    {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
