9726704c9a353cc07ea52c7fc6a4f6eb0b3664f9
matiigodoy <matiigodoy@outlook.com>
2024-10-26 12:15:09 -0300

    Se agregó EF

---
+++ ToDoList.Data/EF/Tarea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList.Data.EF;
+
+public partial class Tarea
+{
+    public int Id { get; set; }
+
+    public string Titulo { get; set; } = null!;
+
+    public string Descripcion { get; set; } = null!;
+
+    public int? IdTablero { get; set; }
+
+    public int? IdEstado { get; set; }
+
+    public virtual Estado? IdEstadoNavigation { get; set; }
+
+    public virtual Tablero? IdTableroNavigation { get; set; }
+}
